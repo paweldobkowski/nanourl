fdc847b8f9026941e53b5b373706df0235c302a7
Paweł Dobkowski <paweldobkowski@gmail.com>
2025-04-04 19:56:47 +0200

    refactor: formatted everything with black

--- nano_url/nano_url/settings.py
+++ nano_url/nano_url/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'nanofier',
-    'rest_framework',
+    "nanofier",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
