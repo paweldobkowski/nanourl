e9f9b515f9305f679e5ecedc15953ad300b620ed
Paweł Dobkowski <paweldobkowski@gmail.com>
2025-04-05 21:23:31 +0200

    feature: added routing to resolve Nano URL

--- nano_url/nanofier/urls.py
+++ nano_url/nanofier/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateNanoURL
+
+from .views import CreateNanoURL, ReverseNanoURL
 
 urlpatterns = [
     path("nanofy/", CreateNanoURL.as_view(), name="nanofy"),
+    path("resolve/", ReverseNanoURL.as_view(), name="resolve"),
 ]
