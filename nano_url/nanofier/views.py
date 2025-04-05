efd850e3b2b01b5224c007f4ef16ba19850870db
Paweł Dobkowski <paweldobkowski@gmail.com>
2025-04-05 21:19:35 +0200

    feature: created a view for reversing the Nano URL back to original URL

--- nano_url/nanofier/views.py
+++ nano_url/nanofier/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 from .models import NanoURL
 
-from .serializers import NanoURLSerializer
+from .serializers import NanoURLSerializer, ReverseNanoURLSerializer
 
 
 class CreateNanoURL(CreateAPIView):
@@ -12,3 +14,16 @@ class CreateNanoURL(CreateAPIView):
 
     queryset = NanoURL.objects.all()
     serializer_class = NanoURLSerializer
+
+
+class ReverseNanoURL(GenericAPIView):
+    """
+    Function that handles POST request, reverses the Nano URL to original URL.
+    """
+
+    serializer_class = ReverseNanoURLSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=HTTP_200_OK)
