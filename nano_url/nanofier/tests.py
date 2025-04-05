0c68bb075b09b692d1920e308621a5a15e29201a
Paweł Dobkowski <paweldobkowski@gmail.com>
2025-04-05 22:52:13 +0200

    test: added basic tests to check if api returns Nano URL and 400 when provided with non existing Nano URL to resolve

--- nano_url/nanofier/tests.py
+++ nano_url/nanofier/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import NanoURL
 
-# Create your tests here.
+
+class URLNanofierTests(APITestCase):
+    def test_nanofy_returns_nano_url(self):
+        """
+        Ensure POST /nanofy/ returns a nano_url
+        """
+
+        response = self.client.post(
+            "/nanofy/", {"original_url": "https://example.com/test"}, format="json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn("nano_url", response.data)
+        self.assertIn("original_url", response.data)
+
+    def test_resolve_fails_for_nonexistent_code(self):
+        """
+        Ensure /resolve/ returns 400 if nano code is invalid
+        """
+
+        fake_url = "http://localhost:8000/fake123/"
+        response = self.client.post("/resolve/", {"nano_url": fake_url}, format="json")
+        print(response.status_code)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("nano_url", response.data)
